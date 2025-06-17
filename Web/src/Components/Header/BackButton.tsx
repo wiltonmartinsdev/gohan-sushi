4deae0f29a409f0fc7ee8c9dbd1a8de3a157d3a8
Wilton Lira Martins <wiltonliramartins@icloud.com>
2025-06-17 12:26:56 -0300

    fix(BackButton): replace useNavigate with Link for navigation

--- Web/src/Components/Header/BackButton.tsx
+++ Web/src/Components/Header/BackButton.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/Components/ui/button";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface BackButtonProps {
 	to?: string;
@@ -12,14 +12,12 @@ export function BackButton({
 	label = "Voltar ao Início",
 	className = "",
 }: BackButtonProps) {
-	const navigate = useNavigate();
-
 	return (
 		<Button
-			onClick={() => navigate(to)}
+			asChild
 			variant="outline"
 			className={`border-[#e60000] text-white hover:bg-[#e60000] hover:text-white cursor-pointer ${className}`}>
-			{label}
+			<Link to={to}>{label}</Link>
 		</Button>
 	);
 }
